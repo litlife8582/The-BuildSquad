19751bcca094ac7c6109e150680a838f97959ec1
Mayukh Sarkar <sarkarmayukh2006@gmail.com>
2026-04-04 23:41:01 +0530

    updated

--- app/src/pages/About.jsx
+++ app/src/pages/About.jsx
@@ -10,7 +10,7 @@ export default function About() {
                     <div className='w-1/2 text-lg px-10 text-justify'>The BuildSquad Construction stands as a premier vertical within the formidable infrastructure division of BuildSquad Groups Ltd., a titan among Indian technology, engineering, construction, manufacturing, and financial services conglomerates. Boasting a heavy-weight global presence, BuildSquad represents the vanguard of Indian industrial capability on the world stage.
 
                     Dedicated to solving complex challenges, BuildSquad addresses pivotal needs within foundational economic sectors. Our portfolio spans the high-impact realms of Hydrocarbon engineering, critical large-scale Infrastructure, sustainable Power generation, complex Process Industries, and advanced Defence systems. We are a trusted partner to nations and private corporations in over 30 countries across five continents. </div>
-                    <div className='w-1/2 pr-10'> <img className='w-full h-[350px] object-cover' src={factoryImage} alt="Factory-image" /></div>
+                    <div className='w-1/2 pr-10'><img className='w-full h-[350px] object-cover' src={factoryImage} alt="Factory-image" /></div>
                 </div>
                 <div className='text-container items-center gap-10 mt-4'>
                     <div className='w-1/2 pl-10'><img className='w-full h-[350px] object-cover' src={factoryImage2} alt="Factory-image 2" /></div>
